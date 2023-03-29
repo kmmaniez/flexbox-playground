9c3c6ce36490852c2c0e40ec653e9b6a333d8c6f
kmmaniez <dzlabsid@gmail.com>
2023-03-29 11:33:21 +0700

    Declaration parent container variable  & add function to switch flex property

---
+++ src/main.js
@@ -0,0 +1,110 @@
+/* Container */
+const container             = document.querySelector('.container')
+
+/* Flex Container Item */
+const flexDirectionBox      = document.querySelectorAll('.direction');
+const flexContentBox        = document.querySelectorAll('.flexcontent');
+const flexItemsBox          = document.querySelectorAll('.flexitems');
+const flexWrapBox           = document.querySelectorAll('.flexwrap');
+
+/* Flex container item event */
+flexDirectionBox.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const {id} = e.target
+        switchFlex(id)
+    })
+})
+
+flexContentBox.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const {id} = e.target
+        switchFlex(id)
+    })
+})
+
+flexItemsBox.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const {id} = e.target
+        switchFlex(id)
+    })
+})
+
+flexWrapBox.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const {id} = e.target
+        switchFlex(id)
+    })
+})
+
+
+/* Function switch flex property (parent/child) */
+function switchFlex(id) {
+    switch (id) {
+
+    // 1. FLEX CONTAINER
+        /* Flex direction */
+        case 'row':
+            container.style.flexDirection = 'row';
+            break;
+        case 'rowrev':
+            container.style.flexDirection = 'row-reverse';
+            break;
+        case 'col':
+            container.style.flexDirection = 'column';
+            break;
+        case 'colrev':
+            container.style.flexDirection = 'column-reverse';
+            break;
+            
+        /* Justify content */
+        case 'between':
+            container.style.justifyContent = 'space-between';
+            break;
+        case 'around':
+            container.style.justifyContent = 'space-around';
+            break;
+        case 'evenly':
+            container.style.justifyContent = 'space-evenly';
+            break;
+        case 'start':
+            container.style.justifyContent = 'start';
+            break;
+        case 'end':
+            container.style.justifyContent = 'end';
+            break;
+
+        /* Align items */
+        case 'center':
+            container.style.alignItems = 'center';
+            break;
+        case 'flexs':
+            container.style.alignItems = 'start';
+            break;
+        case 'flexe':
+            container.style.alignItems = 'end';
+            break; 
+        case 'flexl':
+            container.style.alignItems = 'left';
+            break;
+        case 'flexr':
+            container.style.alignItems = 'right';
+            break;
+        case 'flexstretch':
+            container.style.alignItems = 'stretch';
+            break;
+
+        /* Flex wrap */
+        case 'nowrap':
+            container.style.flexWrap = 'nowrap';
+            break;
+        case 'wrap':
+            container.style.flexWrap = 'wrap';
+            break;
+        case 'wraprev':
+            container.style.flexWrap = 'wrap-reverse';
+            break;
+    
+        default:
+            break;
+    }
+}
